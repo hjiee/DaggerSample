d34dd1dcc64b940974db8be3d391cb20b4bb6f8a
hyojin <jin33032@gmail.com>
2020-03-05 23:58:19 +0900

    UPDATE [ inject main viewmodel ]

--- app/src/main/java/com/hyden/daggersample/MainActivity.kt
+++ app/src/main/java/com/hyden/daggersample/MainActivity.kt
@@ -2,13 +2,19 @@ package com.hyden.daggersample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
 
+    @Inject lateinit var viewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+
+        viewModel.loadLanguage()
     }
 }
